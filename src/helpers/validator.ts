447f4215c8fffeb07e86f6c025f649ec1feb30e0
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-09-20 20:39:54 +0900

    feat: added schema validation workflow

---
+++ src/helpers/validator.ts
@@ -0,0 +1,30 @@
+import { Value } from "@sinclair/typebox/value";
+import { envSchema, envValidator, PluginSettings, pluginSettingsSchema, pluginSettingsValidator } from "../types";
+
+export function validateAndDecodeSchemas(env: object, rawSettings: object) {
+  if (!envValidator.test(env)) {
+    const errors: object[] = [];
+    for (const error of envValidator.errors(env)) {
+      const errorMessage = { path: error.path, message: error.message, value: error.value };
+      console.error(errorMessage);
+      errors.push(errorMessage);
+    }
+    throw new Error(`Invalid environment provided. ${errors}`);
+  }
+  const envDecoded = Value.Decode(envSchema, env || {});
+
+  const settings = Value.Default(pluginSettingsSchema, rawSettings) as PluginSettings;
+  if (!pluginSettingsValidator.test(settings)) {
+    const errors: object[] = [];
+    for (const error of pluginSettingsValidator.errors(settings)) {
+      const errorMessage = { path: error.path, message: error.message, value: error.value };
+      console.error(errorMessage);
+      errors.push(errorMessage);
+    }
+    throw new Error(`Invalid settings provided. ${errors}`);
+  }
+
+  const settingsDecoded = Value.Decode(pluginSettingsSchema, settings);
+
+  return { envDecoded, settingsDecoded };
+}
