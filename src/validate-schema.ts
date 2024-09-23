9e57d63cff834621b09c41310aaab4ec4b76863b
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-09-24 00:29:14 +0900

    chore: added errors

--- src/validate-schema.ts
+++ src/validate-schema.ts
@@ -1,6 +1,5 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
-import { TransformDecodeCheckError, TransformDecodeError } from "@sinclair/typebox/value";
 import { returnDataToKernel } from "./action";
 import { validateAndDecodeSchemas } from "./helpers/validator";
 
@@ -8,16 +7,7 @@ async function main() {
   const payload = github.context.payload.inputs;
 
   payload.env = { ...(payload.env || {}), workflowName: github.context.workflow };
-  try {
-    validateAndDecodeSchemas(payload.env, JSON.parse(payload.settings));
-  } catch (errors) {
-    console.error(errors);
-    if (errors instanceof TransformDecodeCheckError || errors instanceof TransformDecodeError) {
-      throw { errors: [errors.error], payload };
-    } else {
-      throw { errors, payload };
-    }
-  }
+  validateAndDecodeSchemas(payload.env, JSON.parse(payload.settings));
   return { errors: [], payload };
 }
 
@@ -31,9 +21,10 @@ main()
     core.setFailed(e.errors);
     return e;
   })
-  .then(async (payload) => {
-    console.log("returning data to kernel!");
-    await returnDataToKernel(process.env.GITHUB_TOKEN, payload.stateId, { errors: payload.errors }, "configuration_validation");
+  .then(async (errors) => {
+    console.log("returning data to kernel!", errors);
+    const payload = github.context.payload.inputs;
+    await returnDataToKernel(process.env.GITHUB_TOKEN, payload.stateId, errors, "configuration_validation");
   })
   .catch((e) => {
     console.error("Failed to return the data to the kernel.", e);
